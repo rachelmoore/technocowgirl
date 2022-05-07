06a9edda8fb0b64fef77ac653eac8b463757cb70
Rachel Moore <rachel@rachelmoore.net>
2022-05-07 13:16:25 -0700

    spruce up error page

--- packages/techno-chakra/src/components/loading.js
+++ packages/techno-chakra/src/components/loading.js
@@ -5,8 +5,8 @@ import wheel from '../assets/Wheel200px.gif';
 
 const Loading = () => {
     return (
-        <Flex direction="column" align="center">
-            <Image src={wheel} />
+        <Flex direction="column" align="center" m={5}>
+            <Image src={wheel} mb={5} />
             <div>
                 <audio src="https://ia600909.us.archive.org/21/items/tvtunes_29841/Rawhide%20-%20Frankie%20Laine.mp3" controls autoPlay/>
             </div>
@@ -15,17 +15,3 @@ const Loading = () => {
 }
 
 export default Loading
-
-// const spin = keyframes`
-//   0% { transform: rotate(0deg); }
-//   100% { transform: rotate(360deg); }
-// `
-
-// const Spinner = styled.div`
-//   border: 12px solid #eee;
-//   border-top: 12px solid steelblue;
-//   border-radius: 50%;
-//   width: 80px;
-//   height: 80px;
-//   animation: ${spin} 2s linear infinite;
-// `
