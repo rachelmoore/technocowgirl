c98f910c9cc3f86b555f7d621036cfd6b91b4b34
Rachel Moore <rachel@rachelmoore.net>
2023-07-09 10:24:16 -0700

    comments improved

--- packages/techno-chakra/src/components/post.js
+++ packages/techno-chakra/src/components/post.js
@@ -6,9 +6,11 @@ import {
   Stat,
   StatLabel,
   StatHelpText,
-  Button
+  Button,
+  IconButton
 } from "@chakra-ui/react";
 import Link from "@frontity/components/link";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import dayjs from "dayjs";
 import CommentsForm from "./commentsform";
 
@@ -55,69 +57,87 @@ const Post = ({ actions, state, libraries }) => {
 
   if (!data.isFetching) {
     return (
-      <Flex direction="column" align="center" mb={10}>
-        <Head>
-          <title>{post.title.rendered}</title>
-          <meta name="description" content={post.excerpt.rendered} />
-        </Head>
+      <Flex direction="column">
+        <Flex direction="column" align="center" mb={10}>
+          <Head>
+            <title>{post.title.rendered}</title>
+            <meta name="description" content={post.excerpt.rendered} />
+          </Head>
 
-        <PostContainer>
-          <Link link={post.link} style={{textDecoration: "none"}}>
-            <PostTitle>
-              <Heading size="lg" pb={5} pt={5}>
-                <Html2React html={post.title.rendered} />
-              </Heading>
-            </PostTitle>
-            <br />
-          </Link>
-          <PostContent>
-              <Stat>
-                <StatLabel>Author: {author.name}</StatLabel>
-                <StatHelpText>Posted: {formattedDate}</StatHelpText>
-              </Stat>
-              <Html2React html={post.content.rendered} />
-          </PostContent>
-        </PostContainer>
-            
-        {/* {!showComments &&  */}
-          
-        {/* }
-        {!showCommentForm &&  */}
-
-        {/* } */}
-
-        {!!showComments && 
-          <Comments>
-            {commentsKeys.map((commentKey) => {
-              return ( 
-                <Comment>
-                  <img src={comments[commentKey].author_avatar_urls[96]} />
-                  <CommentInfo>
-                    <CommentMeta>
-                      <p>
-                        <strong>Posted:          </strong>
-                        {dayjs(comments[commentKey].date).format("DD MMMM YYYY")}
-                      </p>
-                      <p>
-                        <strong>Author:          </strong>
-                        {comments[commentKey].author_name}
-                      </p>
-                    </CommentMeta>
-                    <p><Html2React html={comments[commentKey].content.rendered} /></p>
-                  </CommentInfo>
-                </Comment>
+          <PostContainer>
+            <Link link={post.link} style={{textDecoration: "none"}}>
+              <PostTitle>
+                <Heading size="lg" pb={5} pt={5}>
+                  <Html2React html={post.title.rendered} />
+                </Heading>
+              </PostTitle>
+              <br />
+            </Link>
+            <PostContent>
+                <Stat>
+                  <StatLabel>Author: {author.name}</StatLabel>
+                  <StatHelpText>Posted: {formattedDate}</StatHelpText>
+                </Stat>
+                <Html2React html={post.content.rendered} />
+            </PostContent>
+          </PostContainer>
+        </Flex>
+        <Flex direction="column" justifyContent="center" alignItems="center" width="100%">
+          <Flex direction="column" justifyContent="center" marginBottom="100px" width="600px">
+          {!!showComments && 
+            <Comments>
+              {commentsKeys.map((commentKey) => {
+                return ( 
+                  <Comment>
+                    <img src={comments[commentKey].author_avatar_urls[96]} />
+                    <CommentInfo>
+                      <CommentMeta>
+                        <p>
+                          <strong>Posted:          </strong>
+                          {dayjs(comments[commentKey].date).format("DD MMMM YYYY")}
+                        </p>
+                        <p>
+                          <strong>Author:          </strong>
+                          {comments[commentKey].author_name}
+                        </p>
+                      </CommentMeta>
+                      <p><Html2React html={comments[commentKey].content.rendered} /></p>
+                    </CommentInfo>
+                  </Comment>
+                )}
               )}
-            )}
-          </Comments>
-        }
+            </Comments>
+          }
 
-        <Button bg="brand.100" color="#FFFFFF" onClick={() => setShowComments(!showComments)} mb={5}>{commentsText}</Button>
 
-        <Button bg="#EE0300" color="#FFFFFF" onClick={() => setShowCommentForm(!showCommentForm)} mb={5}>{commentFormText}</Button>
+          <Flex direction="row" justifyContent="center" mt={5}>
+            <Flex direction="row" justifyContent="center" mr={3}>
+              <Button bg="brand.100" color="#FFFFFF" onClick={() => setShowComments(!showComments)} mb={5}>{commentsText}</Button>
+            </Flex>
             
-        {!!showCommentForm && 
-          <CommentsForm actions={actions} state={state} postId={data.id} />
-        }
+            {showCommentForm === false &&
+            <Flex direction="row" justifyContent="center" ml={3}>
+              <Button bg="#EE0300" color="#FFFFFF" onClick={() => setShowCommentForm(!showCommentForm)} mb={5}>{commentFormText}</Button>
+            </Flex>
+            }
+          </Flex>
+
+          {showCommentForm === true &&
+          <Flex direction="row-reverse" justifyContent="center" width="100%">
+            <Flex direction="row-reverse" width="300px">
+              <IconButton onClick={() => setShowCommentForm(!showCommentForm)} background="transparent" color="#EE0300" _hover={{ color: "#FFFFFF" }} size="md" icon={<AiOutlineCloseCircle size="lg" />} />
+            </Flex>
+          </Flex>
+          }
+
+          <Flex direction="column" justifyContent="center" alignItems="center">
+          {!!showCommentForm && 
+            <CommentsForm actions={actions} state={state} postId={data.id} />
+          }
+          </Flex>
+        </Flex>
+        </Flex>
+
       </Flex>
     )
   }
@@ -200,7 +220,7 @@ const CommentInfo = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 1em;
+  /* margin-bottom: 1em; */
   padding: 0.5em;
   font-size: 1em;
   /* border-left: 4px solid #AD9044; */
@@ -216,20 +236,26 @@ const CommentInfo = styled.div`
 const CommentMeta = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 1em;
+  /* margin-bottom: 1em;
   margin-left: 1em;
   padding: 0.5em;
   font-size: 1em;
   /* border-left: 4px solid #AD9044; */
   & > p {
     margin: 0;
-  }
+  } */
 `
 
 const Comment = styled.div`
   margin-top: 20px;
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  color: #FFFFFF;
+  border: 2px solid #FFFFFF;
+  border-radius: 50px;
   & > img {
     border-radius: 50px;
     height: 96px;
@@ -238,5 +264,9 @@ const Comment = styled.div`
 `
 
 const Comments = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
   margin-top: 20px;
 `
